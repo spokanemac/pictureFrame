3586efe7603318b2aee86418e7159255c9b2e796
JD <spokanemac@users.noreply.github.com>
2022-01-27 16:15:11 -0800

    Initial version of the pictureFrame.sh launcher
    This is a little sloppy, but it helps us set the rclone environment and kickoff python to run the script.

--- pictureFrame.sh
+++ pictureFrame.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
+cd "$(dirname "$0")";
+CWD="$(pwd)"
+WAI="$(whoami)"
+echo $CWD
+echo $WAI
 
+RCLONE_CONFIG=/home/pi/.config/rclone/rclone.conf
+export RCLONE_CONFIG
+python3 /home/pi/pictureFrame.py
